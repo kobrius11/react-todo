814e6092301cf4e15d93ef56e44c660d57527d13
kobrius11 <kobrius11@gmail.com>
2025-01-02 20:33:02 +0200

    initial todo app

---
+++ src/data/todo.ts
@@ -0,0 +1,46 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Organize my workspace for better productivity",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Plan the grocery shopping for the upcoming week",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Research destinations for a weekend getaway",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Schedule a catch-up call with an old friend",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Clean out the inbox and prioritize emails",
+    completed: false,
+  },
+  {
+    id: 6,
+    title: "Prepare a healthy meal for dinner tonight",
+    completed: false,
+  },
+  { id: 7, title: "Complete a 20-minute workout session", completed: false },
+  { id: 8, title: "Draft a budget plan for the month", completed: false },
+  {
+    id: 9,
+    title: "Read a chapter of the book I started last week",
+    completed: false,
+  },
+  {
+    id: 10,
+    title: "Update my to-do list for tomorrow's tasks",
+    completed: false,
+  },
+];
